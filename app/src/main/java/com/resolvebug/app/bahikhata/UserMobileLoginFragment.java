5e4d4a5d2b3d78a2401bc5f9ecf7e3fb033a909f
Aishwarya Gupta <resolvebug@gmail.com>
2018-10-27 16:07:17 +0530

    [27102018]-mobile login implemented

--- app/src/main/java/com/resolvebug/app/bahikhata/UserMobileLoginFragment.java
+++ app/src/main/java/com/resolvebug/app/bahikhata/UserMobileLoginFragment.java
@@ -1,14 +1,30 @@
 package com.resolvebug.app.bahikhata;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -16,9 +32,14 @@ import android.widget.EditText;
  */
 public class UserMobileLoginFragment extends Fragment {
 
-    private Button phoneLoginButton;
-    private EditText signInMobile;
-    private TextInputLayout otpInput;
+    private FirebaseAuth firebaseAuth;
+    private Button otpRequestButton;
+    private EditText userPhoneNumber;
+    private EditText signInOtp;
+    private TextInputLayout oneTimePassword;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks callBacks;
+    private String mVerificationId;
+    private PhoneAuthProvider.ForceResendingToken mResendToken;
 
     public UserMobileLoginFragment() {
     }
@@ -27,26 +48,85 @@ public class UserMobileLoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.fragment_user_mobile_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_user_mobile_login, container, false);
         initializeComponents(view);
-        setOnButtonClickListeners();
+        setOnOtpButtonClickListeners();
         return view;
     }
 
     private void initializeComponents(View view) {
-        phoneLoginButton = view.findViewById(R.id.phoneLoginButton);
-        signInMobile = view.findViewById(R.id.signInMobile);
-        otpInput = view.findViewById(R.id.otpInput);
+        otpRequestButton = view.findViewById(R.id.otpRequestButton);
+        signInOtp = view.findViewById(R.id.signInOtp);
+        userPhoneNumber = view.findViewById(R.id.userPhoneNumber);
+        userPhoneNumber.requestFocus();
+        oneTimePassword = view.findViewById(R.id.oneTimePassword);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
 
-    private void setOnButtonClickListeners() {
-        phoneLoginButton.setOnClickListener(new View.OnClickListener() {
+    private void setOnOtpButtonClickListeners() {
+        otpRequestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                otpInput.setVisibility(view.VISIBLE);
-                otpInput.requestFocus();
-                phoneLoginButton.setText("Sign In");
+                String mobileNumber = userPhoneNumber.getText().toString().trim();
+                String otp = signInOtp.getText().toString().trim();
+                if (!mobileNumber.equals("")) {
+                    oneTimePassword.setVisibility(view.VISIBLE);
+                    oneTimePassword.requestFocus();
+                    otpRequestButton.setText("Sign In");
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            mobileNumber,
+                            60,
+                            TimeUnit.SECONDS,
+                            getActivity(),
+                            callBacks
+                    );
+                }
+                if(!otp.equals("")){
+                    PhoneAuthCredential credential = PhoneAuthProvider.getCredential(mVerificationId, otp);
+                    signInWithPhoneAuthCredential(credential);
+                }
             }
         });
+
+        callBacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+            }
+
+            @Override
+            public void onCodeSent(String verificationId,
+                                   PhoneAuthProvider.ForceResendingToken token) {
+                mVerificationId = verificationId;
+                mResendToken = token;
+            }
+        };
     }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        firebaseAuth.signInWithCredential(credential)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = task.getResult().getUser();
+                            startActivity(new Intent(getActivity(), MainActivity.class));
+                            getActivity().finish();
+                            // ...
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                            }
+                        }
+                    }
+                });
+    }
+
 }
