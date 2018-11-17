942474db06e6211663944a8449e0ecedc1ec3479
Aishwarya Gupta <resolvebug@gmail.com>
2018-11-17 22:27:47 +0530

    [17112018]-redesigning activities and fragments - opening properly - part 3

--- app/src/main/java/com/resolvebug/app/bahikhata/MainActivity.java
+++ app/src/main/java/com/resolvebug/app/bahikhata/MainActivity.java
@@ -93,33 +93,28 @@ public class MainActivity extends AppCompatActivity implements DatePickerDialog.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // main
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        loadLocale();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,
                 WindowManager.LayoutParams.FLAG_SECURE);
+        setContentView(R.layout.activity_main);
         userAuthentication();
-
-
-        // new and edit fragment
         initialize();
-        setTitleFont();
-        setAdView();
-        performDBOperations();
-        initializeDatePickerFragment();
-        setHomeButton();
 
-        // settings
-        loadLocale();
         changeLanguage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showLanguageChangeDialog();
             }
         });
-        exportCSV();
-        setUserLogoutButton();
 
+        setTitleFont();
+        setAdView();
+        performDBOperations();
+        exportCSV();
+        initializeDatePickerFragment();
+        setHomeButton();
+        setUserLogoutButton();
 
 //        fab = findViewById(R.id.main_fab);
 //        fab1 = findViewById(R.id.main_fab1);
