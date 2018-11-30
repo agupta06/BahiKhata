a62bcba0649a77ade2415822c6dcf9704a0e5e6a
asrivastav-BSFT <asrivastav@broadsoft.com>
2018-11-30 21:43:50 +0530

    [30112018]-Removing unused imports

--- app/src/main/java/com/resolvebug/app/bahikhata/UserSignInFragment.java
+++ app/src/main/java/com/resolvebug/app/bahikhata/UserSignInFragment.java
@@ -1,7 +1,6 @@
 package com.resolvebug.app.bahikhata;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
