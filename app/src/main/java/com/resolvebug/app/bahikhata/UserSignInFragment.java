04f9a604163b48f83a1ccd5b80defb5095f9327a
ankita <ankitasrivastav94@gmail.com>
2018-11-30 22:03:01 +0530

    [30112018]-Reverting imports

--- app/src/main/java/com/resolvebug/app/bahikhata/UserSignInFragment.java
+++ app/src/main/java/com/resolvebug/app/bahikhata/UserSignInFragment.java
@@ -1,6 +1,7 @@
 package com.resolvebug.app.bahikhata;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
